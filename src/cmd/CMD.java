35af871520e300d6802ae78e9526671e87c94e95
Nickyi175 <124185028+Nickyi175@users.noreply.github.com>
2023-08-25 15:03:03 -0600

    cmd

--- src/cmd/CMD.java
+++ src/cmd/CMD.java
@@ -1,20 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package cmd;
 
-/**
- *
- * @author tomea
- */
-public class CMD {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class CMD {
+   Scanner leer = new Scanner(System.in).useDelimiter("\n"); 
+   File mifile;
+   String direccion;
+
+    public CMD() {
+        this.mifile = new File(direccion);
     }
+void Escribir(File mifile) throws IOException{
+//    if(.equalsIgnoreCase("Escribir<wr>")){
+    if (!mifile.isDirectory()) {
+            System.out.println("No existe");
+            return;
+        }
+    FileWriter writer = new FileWriter(mifile, true);
+        String line;
+    while (true) {
+        line = leer.nextLine();
+        if (line.isEmpty()) {
+            break;
+        }
+        writer.write(line);
+        writer.write(System.lineSeparator());
+    }
+        writer.close();
+//}
+}
+
+
+ void Leer(File mifile){
+    try {
+//        if(.equalsIgnoreCase("Leer<rd>")){
+        if (!mifile.exists()) {
+            System.out.println("Archivo no encontrado");
+            return;
+        }
+        BufferedReader reader = new BufferedReader(new FileReader(mifile));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
+//    }
+    } catch (IOException ex) {
+        Logger.getLogger(CMD.class.getName()).log(Level.SEVERE, null, ex);
+    }
+ }
+ 
+ static void Time(){
+//     if(.equalsIgnoreCase("Time")){
+     SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+     System.out.println("Current time: " + timeFormat.format(new Date()));
+// }              
+ }
+//    
     
 }
